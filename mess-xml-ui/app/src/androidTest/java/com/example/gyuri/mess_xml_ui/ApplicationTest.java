c38e918665ede8de0a953c9e613e55e93d7f7611
bgyuri76 <bgyuridev@gmail.com>
2016-07-28 15:38:08 +0300

    working ui

---
+++ mess-xml-ui/app/src/androidTest/java/com/example/gyuri/mess_xml_ui/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.gyuri.mess_xml_ui;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
